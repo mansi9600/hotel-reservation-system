190b2261b9eef191742a365a8b1c042a419a4126
Mansi Gupta <mansiguptamg44@gmail.com>
2026-06-15 17:51:02 +0530

    Add Room COntroller Integration Test Fixes #11

--- src/test/java/controller/RoomController.java
+++ src/test/java/controller/RoomController.java
@@ -1,1 +1,12 @@
+package com.hotel.hotel_system.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomControllerTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
